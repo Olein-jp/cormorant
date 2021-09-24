d27cc9551f1273190a5901f93de37beaa6e3dfed
KojiKuno <koji.kuno@gmail.com>
2021-09-25 08:44:52 +0900

    Created a part of register block style basic section

--- inc/block-styles/block-styles.php
+++ inc/block-styles/block-styles.php
@@ -1,2 +1,26 @@
 <?php
-/* silence is gold */
+/**
+ * Block Styles
+ * Register original block styles.
+ */
+
+if ( ! function_exists( 'cormorant_register_block_styles' ) ) {
+	function cormorant_register_block_styles() {
+
+		if ( ! function_exists( 'register_block_style' ) ) {
+			return;
+		}
+
+		// Sample style
+		register_block_style(
+			'core/heading',
+			array(
+				'name'  => 'cormorant-sample-block-style',
+				'label' => esc_html__( 'Sample', 'cormorant' ),
+			)
+		);
+
+	}
+
+	add_action( 'init', 'cormorant_register_block_styles' );
+}
