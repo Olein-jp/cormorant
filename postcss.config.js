bd64f4a3d7dca2283ad02814a4a9c1f169875519
KojiKuno <koji.kuno@gmail.com>
2021-09-27 00:21:03 +0900

    Added autoprefixer and PostCSS

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    plugins: [
+        require('postcss-import')(),
+        require('autoprefixer')({
+            cascade: false,
+        }),
+        require('css-mqpacker')({
+            sort: true,
+        }),
+        require('postcss-mq-optimize')
+    ]
+}
