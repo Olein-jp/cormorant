d4361bde6126a7b628d1d22b8b5d292d2c862939
Olein-jp <koji.kuno@gmail.com>
2022-09-28 16:42:07 +0900

    :white_check_mark: `cormorant/section-left-text-right-image-no-bg`

---
+++ patterns/section-left-text-right-image-no-bg.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Title: Left Text - Right Image - no background
+ * Slug: cormorant/section-left-text-right-image-no-bg
+ * Categories: section
+ * Description: Left text right image section (no background)
+ * Block Types: core/media-text, core/paragraph, core/buttons, core/heading
+ *
+ * @author Koji Kuno
+ * @package cormorant
+ * @since 1.0.0
+ */
+?>
+<!-- wp:media-text {"align":"full","mediaPosition":"right","mediaId":2003,"mediaLink":"http://cormorant.wp/?attachment_id=2003","mediaType":"image","verticalAlignment":"top","imageFill":true,"className":"cormorant-block-pattern-section-left-text-right-image"} -->
+<div class="wp-block-media-text alignfull has-media-on-the-right is-stacked-on-mobile is-vertically-aligned-top is-image-fill cormorant-block-pattern-section-left-text-right-image"><div class="wp-block-media-text__content">
+		<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":"1.2"},"spacing":{"margin":{"bottom":"var:preset|spacing|60","top":"0"}}},"className":"is-style-default"} -->
+		<h3 class="is-style-default" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--60);font-style:normal;font-weight:700;line-height:1.2">Nascetur aenean velit penatibus lectus id</h3>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|60"}}}} -->
+		<p style="margin-bottom:var(--wp--preset--spacing--60)">Vehicula lorem faucibus mauris quam commodo enim dapibus nisi pellentesque etiam senectus justo ullamcorper, ligula maecenas erat aliquam dolor lacinia hac congue eu quisque varius. Donec netus integer faucibus orci sem convallis tortor ultricies vitae mollis nascetur torquent, nunc mi nulla cursus felis diam ligula id viverra turpis. Platea dolor morbi pharetra tristique auctor adipiscing eros vitae, aliquet semper donec fringilla ad gravida lectus ridiculus natoque, risus mauris bibendum posuere mi egestas inceptos.</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+		<div class="wp-block-buttons">
+			<!-- wp:button {"width":75,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}},"className":"is-style-outline","fontSize":"large"} -->
+			<div class="wp-block-button has-custom-width wp-block-button__width-75 has-custom-font-size is-style-outline has-large-font-size" style="font-style:normal;font-weight:700">
+				<a class="wp-block-button__link wp-element-button" style="padding-top:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20)">Button</a>
+			</div>
+			<!-- /wp:button -->
+		</div>
+		<!-- /wp:buttons -->
+	</div>
+	<figure class="wp-block-media-text__media" style="background-image:url(http://cormorant.wp/wp-content/uploads/2022/09/StockSnap_QYACYW59XD-1024x768.jpg);background-position:50% 50%">
+		<img src="http://cormorant.wp/wp-content/uploads/2022/09/StockSnap_QYACYW59XD-1024x768.jpg" alt="sample image" class="wp-image-2003 size-full"/>
+	</figure>
+</div>
+<!-- /wp:media-text -->
