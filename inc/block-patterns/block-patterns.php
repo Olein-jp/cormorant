ebf868e3cc5c41190c609992470051d76609944b
Olein-jp <koji.kuno@gmail.com>
2022-09-28 15:23:55 +0900

    :recycle: `block-pattens.php`

--- inc/block-patterns/block-patterns.php
+++ inc/block-patterns/block-patterns.php
@@ -12,19 +12,19 @@ if ( ! function_exists( 'cormorant_register_block_categories' ) ) {
 	 */
 	function cormorant_register_block_pattern_categories() {
 		$cormorant_block_pattern_categories = array(
-			'section'    => array( 'label' => __( 'Section', 'cormorant' ) ),
-			'hero' => array( 'label' => __( 'Hero', 'cormorant' ) ),
-			'column' => array( 'label' => __( 'Column', 'cormorant' ) ),
-			'banner' => array( 'label' => __( 'Banner', 'cormorant' ) ),
-			'cta' => array( 'label' => __( 'CTA', 'cormorant' ) ),
-			'qa' => array( 'label' => __( 'QA', 'cormorant' ) ),
-			'query' => array( 'label' => __( 'Query', 'cormorant' ) ),
-			'page' => array( 'label' => __( 'Page', 'cormorant' ) ),
-			'header' => array( 'label' => __( 'Header', 'cormorant' ) ),
-			'footer' => array( 'label' => __( 'Footer', 'cormorant' ) ),
+			'section'   => array( 'label' => __( 'Section', 'cormorant' ) ),
+			'hero'      => array( 'label' => __( 'Hero', 'cormorant' ) ),
+			'column'    => array( 'label' => __( 'Column', 'cormorant' ) ),
+			'banner'    => array( 'label' => __( 'Banner', 'cormorant' ) ),
+			'cta'       => array( 'label' => __( 'CTA', 'cormorant' ) ),
+			'qa'        => array( 'label' => __( 'QA', 'cormorant' ) ),
+			'query'     => array( 'label' => __( 'Query', 'cormorant' ) ),
+			'page'      => array( 'label' => __( 'Page', 'cormorant' ) ),
+			'header'    => array( 'label' => __( 'Header', 'cormorant' ) ),
+			'footer'    => array( 'label' => __( 'Footer', 'cormorant' ) ),
 			'paragraph' => array( 'label' => __( 'Paragraph', 'cormorant' ) ),
-			'gallery' => array( 'label' => __( 'Gallery', 'cormorant' ) ),
-			'button' => array( 'label' => __( 'Button', 'cormorant' ) ),
+			'gallery'   => array( 'label' => __( 'Gallery', 'cormorant' ) ),
+			'button'    => array( 'label' => __( 'Button', 'cormorant' ) ),
 		);
 		/**
 		 * Filter to add block pattern categories
@@ -42,33 +42,3 @@ if ( ! function_exists( 'cormorant_register_block_categories' ) ) {
 	}
 	add_action( 'init', 'cormorant_register_block_pattern_categories' );
 }
-
-// if ( ! function_exists( 'cormorant_register_block_patterns' ) ) {
-// **
-// * Register Block Patterns
-// *
-// * @since 1.0.0
-// */
-// function cormorant_register_block_patterns() {
-// $cormorant_block_patterns = array();
-//
-// **
-// * Filter to add block pattern
-// */
-// $cormorant_block_patterns = apply_filters( 'cormorant_register_block_patterns', $cormorant_block_patterns );
-//
-// foreach ( $cormorant_block_patterns as $block_pattern ) {
-// $block_pattern_file = untrailingslashit( plugin_dir_path( __FILE__ ) ) . '/patterns/' . $block_pattern . '.php';
-//
-// **
-// * Register Block Pattern from $cormorant_block_patterns
-// */
-// register_block_pattern(
-// 'cormorant/' . $block_pattern,
-// require $block_pattern_file
-// );
-// }
-// }
-//
-// add_action( 'init', 'cormorant_register_block_patterns' );
-// }
