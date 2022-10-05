5ffe4faf3842a82b325c76932b60b06d507ad2b6
Olein-jp <koji.kuno@gmail.com>
2022-10-05 12:27:01 +0900

    :boom: Changed spacing unit number

---
+++ patterns/column-2-dark.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: 2 Column Dark
+ * Slug: cormorant/column-2-dark
+ * Categories: column
+ * Description: 2 Columns styled dark
+ * Block Types: core/group, core/columns, core/paragraph, core/buttons, core/heading
+ *
+ * @author Koji Kuno
+ * @package cormorant
+ * @since 1.0.0
+ */
+?>
