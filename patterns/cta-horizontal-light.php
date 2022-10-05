e2b64c920d9a542f6bad88792e784c12d5679238
Olein-jp <koji.kuno@gmail.com>
2022-10-05 11:43:06 +0900

    :sparkles: Added and Fixed block patterns in CTA

---
+++ patterns/cta-horizontal-light.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Title: CTA Horizontal Light
+ * Slug: cormorant/cta-horizontal-light
+ * Categories: cta
+ * Description: Call To Action styled Horizontal light
+ * Block Types: core/group, core/columns, core/paragraph, core/buttons,
+ *
+ * @author Koji Kuno
+ * @package cormorant
+ * @since 1.0.0
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|X-large","right":"var:preset|spacing|60","bottom":"var:preset|spacing|X-large","left":"var:preset|spacing|60"},"blockGap":"0"}},"backgroundColor":"background","textColor":"foreground","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-foreground-color has-background-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--x-large);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--x-large);padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":3} -->
+		<h3 class="has-text-align-center"><strong>Contact Us</strong></h3>
+		<!-- /wp:heading -->
+
+		<!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"}}}} -->
+		<div class="wp-block-columns are-vertically-aligned-center" style="margin-top:var(--wp--preset--spacing--x-small)"><!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
+			<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%"><!-- wp:paragraph -->
+				<p>Diam elit magnis faucibus arcu duis pulvinar malesuada placerat aenean cubilia purus risus sem, luctus in dictum himenaeos urna id commodo sollicitudin iaculis cursus per. Habitant tortor non nascetur lacus enim rutrum fringilla vivamus integer litora vitae nostra</p>
+				<!-- /wp:paragraph --></div>
+			<!-- /wp:column -->
+
+			<!-- wp:column {"verticalAlignment":"center","width":"33.33%"} -->
+			<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.33%"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"blockGap":"0"}},"fontSize":"medium"} -->
+				<div class="wp-block-buttons has-custom-font-size has-medium-font-size"><!-- wp:button {"className":"is-style-cormorant-foreground-outline-square-button"} -->
+					<div class="wp-block-button is-style-cormorant-foreground-outline-square-button"><a class="wp-block-button__link wp-element-button">Contact Us</a></div>
+					<!-- /wp:button --></div>
+				<!-- /wp:buttons --></div>
+			<!-- /wp:column --></div>
+		<!-- /wp:columns --></div>
+	<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/cta-vertical-dark.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Title: CTA Vertical Dark
+ * Slug: cormorant/cta-vertical-dark
+ * Categories: cta
+ * Description: Call To Action styled Vertical dark
+ * Block Types: core/group, core/paragraph, core/buttons,
+ *
+ * @author Koji Kuno
+ * @package cormorant
+ * @since 1.0.0
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|X-large","right":"var:preset|spacing|60","bottom":"var:preset|spacing|X-large","left":"var:preset|spacing|60"},"blockGap":"0"}},"backgroundColor":"foreground","textColor":"background","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-background-color has-foreground-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--x-large);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--x-large);padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+	<div class="wp-block-group"><!-- wp:heading {"level":3} -->
+		<h3><strong>Contact Us</strong></h3>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph -->
+		<p>Diam elit magnis faucibus arcu duis pulvinar malesuada placerat aenean cubilia purus risus sem, luctus in dictum himenaeos urna id commodo sollicitudin iaculis cursus per. Habitant tortor non nascetur lacus enim rutrum fringilla vivamus integer litora vitae nostra</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"blockGap":"0"}},"fontSize":"medium"} -->
+		<div class="wp-block-buttons has-custom-font-size has-medium-font-size"><!-- wp:button {"className":"is-style-cormorant-background-outline-square-button"} -->
+			<div class="wp-block-button is-style-cormorant-background-outline-square-button"><a class="wp-block-button__link wp-element-button">Contact Us</a></div>
+			<!-- /wp:button --></div>
+		<!-- /wp:buttons --></div>
+	<!-- /wp:group --></div>
+<!-- /wp:group -->
