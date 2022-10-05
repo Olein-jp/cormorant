b4b39284d40020b42fa304c88af559edead322a6
Olein-jp <koji.kuno@gmail.com>
2022-10-05 23:10:58 +0900

    :sparkles: Added price table block patterns

---
+++ patterns/column-3-price-table-light.php
@@ -0,0 +1,198 @@
+<?php
+/**
+ * Title: 3 Column Price Table Light
+ * Slug: cormorant/column-3-price-table-light
+ * Categories: cormorant-column
+ * Description: 3 Columns for price table styled light
+ * Block Types: core/group, core/columns, core/paragraph, core/buttons, core/heading, core/separator
+ *
+ * @author Koji Kuno
+ * @package cormorant
+ * @since 1.0.0
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|X-large","bottom":"var:preset|spacing|X-large"}}},"backgroundColor":"background","textColor":"foreground","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-foreground-color has-background-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--x-large)"><!-- wp:heading {"textAlign":"center","fontSize":"extra-large"} -->
+	<h2 class="has-text-align-center has-extra-large-font-size">Price Table</h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:paragraph {"align":"center"} -->
+	<p class="has-text-align-center">Ornare quisque tincidunt elementum nulla enim lacus</p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:columns {"align":"wide"} -->
+	<div class="wp-block-columns alignwide"><!-- wp:column {"style":{"border":{"width":"1px"}},"borderColor":"foreground"} -->
+		<div class="wp-block-column has-border-color has-foreground-border-color" style="border-width:1px"><!-- wp:group {"style":{"spacing":{"padding":{"top":"20px","right":"20px","bottom":"20px","left":"20px"}}},"backgroundColor":"foreground","textColor":"background","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-background-color has-foreground-background-color has-text-color has-background" style="padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
+				<h3 class="has-text-align-center has-medium-font-size"><strong>Stardard</strong> - $29.9</h3>
+				<!-- /wp:heading --></div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:group {"style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"constrained"}} -->
+				<div class="wp-block-group"><!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">Recommended Point</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:separator {"className":"is-style-wide"} -->
+					<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+					<!-- /wp:separator -->
+
+					<!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">Recommended Point</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:separator {"className":"is-style-wide"} -->
+					<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+					<!-- /wp:separator -->
+
+					<!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">Recommended Point</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:separator {"className":"is-style-wide"} -->
+					<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+					<!-- /wp:separator -->
+
+					<!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">Recommended Point</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:separator {"className":"is-style-wide"} -->
+					<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+					<!-- /wp:separator -->
+
+					<!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">Recommended Point</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:separator {"className":"is-style-wide"} -->
+					<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+					<!-- /wp:separator --></div>
+				<!-- /wp:group -->
+
+				<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+				<div class="wp-block-buttons"><!-- wp:button {"width":100,"className":"is-style-cormorant-foreground-filled-square-button"} -->
+					<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-cormorant-foreground-filled-square-button"><a class="wp-block-button__link wp-element-button">More Detail</a></div>
+					<!-- /wp:button --></div>
+				<!-- /wp:buttons --></div>
+			<!-- /wp:group --></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"border":{"width":"1px"}},"borderColor":"foreground"} -->
+		<div class="wp-block-column has-border-color has-foreground-border-color" style="border-width:1px"><!-- wp:group {"style":{"spacing":{"padding":{"top":"20px","right":"20px","bottom":"20px","left":"20px"}}},"backgroundColor":"foreground","textColor":"background","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-background-color has-foreground-background-color has-text-color has-background" style="padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
+				<h3 class="has-text-align-center has-medium-font-size"><strong>Premium</strong> - $69.9</h3>
+				<!-- /wp:heading --></div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:group {"style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"constrained"}} -->
+				<div class="wp-block-group"><!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">Recommended Point</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:separator {"className":"is-style-wide"} -->
+					<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+					<!-- /wp:separator -->
+
+					<!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">Recommended Point</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:separator {"className":"is-style-wide"} -->
+					<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+					<!-- /wp:separator -->
+
+					<!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">Recommended Point</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:separator {"className":"is-style-wide"} -->
+					<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+					<!-- /wp:separator -->
+
+					<!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">Recommended Point</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:separator {"className":"is-style-wide"} -->
+					<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+					<!-- /wp:separator -->
+
+					<!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">Recommended Point</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:separator {"className":"is-style-wide"} -->
+					<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+					<!-- /wp:separator --></div>
+				<!-- /wp:group -->
+
+				<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+				<div class="wp-block-buttons"><!-- wp:button {"width":100,"className":"is-style-cormorant-foreground-filled-square-button"} -->
+					<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-cormorant-foreground-filled-square-button"><a class="wp-block-button__link wp-element-button">More Detail</a></div>
+					<!-- /wp:button --></div>
+				<!-- /wp:buttons --></div>
+			<!-- /wp:group --></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"border":{"width":"1px"}},"borderColor":"foreground"} -->
+		<div class="wp-block-column has-border-color has-foreground-border-color" style="border-width:1px"><!-- wp:group {"style":{"spacing":{"padding":{"top":"20px","right":"20px","bottom":"20px","left":"20px"}}},"backgroundColor":"foreground","textColor":"background","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-background-color has-foreground-background-color has-text-color has-background" style="padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:heading {"textAlign":"center","level":3,"fontSize":"medium"} -->
+				<h3 class="has-text-align-center has-medium-font-size"><strong>Business</strong> - $99.9</h3>
+				<!-- /wp:heading --></div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:group {"style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"constrained"}} -->
+				<div class="wp-block-group"><!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">Recommended Point</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:separator {"className":"is-style-wide"} -->
+					<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+					<!-- /wp:separator -->
+
+					<!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">Recommended Point</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:separator {"className":"is-style-wide"} -->
+					<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+					<!-- /wp:separator -->
+
+					<!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">Recommended Point</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:separator {"className":"is-style-wide"} -->
+					<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+					<!-- /wp:separator -->
+
+					<!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">Recommended Point</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:separator {"className":"is-style-wide"} -->
+					<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+					<!-- /wp:separator -->
+
+					<!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">Recommended Point</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:separator {"className":"is-style-wide"} -->
+					<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+					<!-- /wp:separator --></div>
+				<!-- /wp:group -->
+
+				<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+				<div class="wp-block-buttons"><!-- wp:button {"width":100,"className":"is-style-cormorant-foreground-filled-square-button"} -->
+					<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-cormorant-foreground-filled-square-button"><a class="wp-block-button__link wp-element-button">More Detail</a></div>
+					<!-- /wp:button --></div>
+				<!-- /wp:buttons --></div>
+			<!-- /wp:group --></div>
+		<!-- /wp:column --></div>
+	<!-- /wp:columns --></div>
+<!-- /wp:group -->
