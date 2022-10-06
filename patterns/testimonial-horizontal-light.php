5de1d866aa1d3c423031a97b5f8107c37955de54
Olein-jp <koji.kuno@gmail.com>
2022-10-06 09:53:29 +0900

    :sparkles: Added testimonial block patterns

---
+++ patterns/testimonial-horizontal-light.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Title: Horizontal Light
+ * Slug: cormorant/testimonial-horizontal-light
+ * Categories: cormorant-testimonial
+ * Description: Horizontal styled light
+ * Keywords: cormorant
+ * Block Types: core/group, core/columns, core/paragraph, core/paragraph
+ *
+ * @author Koji Kuno
+ * @package cormorant
+ * @since 1.0.0
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|X-large","bottom":"var:preset|spacing|X-large"}}},"backgroundColor":"background","textColor":"foreground","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-foreground-color has-background-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--x-large)"><!-- wp:columns {"align":"wide"} -->
+	<div class="wp-block-columns alignwide"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","right":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}}},"backgroundColor":"foreground","textColor":"background"} -->
+		<div class="wp-block-column has-background-color has-foreground-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+			<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"0"}},"fontSize":"huge"} -->
+				<p class="has-huge-font-size" style="line-height:0">“</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph {"align":"center"} -->
+				<p class="has-text-align-center">Consequat duis facilisi rutrum ultricies montes habitant vel sagittis, himenaeos conubia tempor ligula efficitur potenti etiam felis vehicula, tristique aenean maximus facilisis dictum scelerisque commodo. Hendrerit aenean venenatis pellentesque blandit laoreet ipsum inceptos diam curae pulvinar, dolor ultricies rhoncus penatibus in commodo cursus nunc</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph -->
+				<p><strong><em>-- Koji Kuno, Developer</em></strong></p>
+				<!-- /wp:paragraph --></div>
+			<!-- /wp:group --></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","right":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}}},"backgroundColor":"foreground","textColor":"background"} -->
+		<div class="wp-block-column has-background-color has-foreground-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+			<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"0"}},"fontSize":"huge"} -->
+				<p class="has-huge-font-size" style="line-height:0">“</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph {"align":"center"} -->
+				<p class="has-text-align-center">Consequat duis facilisi rutrum ultricies montes habitant vel sagittis, himenaeos conubia tempor ligula efficitur potenti etiam felis vehicula, tristique aenean maximus facilisis dictum scelerisque commodo. Hendrerit aenean venenatis pellentesque blandit laoreet ipsum inceptos diam curae pulvinar, dolor ultricies rhoncus penatibus in commodo cursus nunc</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph {"fontSize":"small"} -->
+				<p class="has-small-font-size"><strong><em>-- Koji Kuno, Developer</em></strong></p>
+				<!-- /wp:paragraph --></div>
+			<!-- /wp:group --></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","right":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}}},"backgroundColor":"foreground","textColor":"background"} -->
+		<div class="wp-block-column has-background-color has-foreground-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+			<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"0"}},"fontSize":"huge"} -->
+				<p class="has-huge-font-size" style="line-height:0">“</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph {"align":"center"} -->
+				<p class="has-text-align-center">Consequat duis facilisi rutrum ultricies montes habitant vel sagittis, himenaeos conubia tempor ligula efficitur potenti etiam felis vehicula, tristique aenean maximus facilisis dictum scelerisque commodo. Hendrerit aenean venenatis pellentesque blandit laoreet ipsum inceptos diam curae pulvinar, dolor ultricies rhoncus penatibus in commodo cursus nunc</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph {"fontSize":"small"} -->
+				<p class="has-small-font-size"><strong><em>-- Koji Kuno, Developer</em></strong></p>
+				<!-- /wp:paragraph --></div>
+			<!-- /wp:group --></div>
+		<!-- /wp:column --></div>
+	<!-- /wp:columns --></div>
+<!-- /wp:group -->
---
+++ patterns/testimonial-vertical-dark.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Title: Vertical Dark
+ * Slug: cormorant/testimonial-vertical-dark
+ * Categories: cormorant-testimonial
+ * Description: Vertical styled dark
+ * Keywords: cormorant
+ * Block Types: core/group, core/columns, core/paragraph, core/paragraph
+ *
+ * @author Koji Kuno
+ * @package cormorant
+ * @since 1.0.0
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|X-large","right":"0","bottom":"var:preset|spacing|X-large","left":"0"}}},"backgroundColor":"foreground","textColor":"background","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-background-color has-foreground-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--x-large);padding-right:0;padding-bottom:var(--wp--preset--spacing--x-large);padding-left:0"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|large"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+	<div class="wp-block-group alignwide"><!-- wp:columns {"align":"wide"} -->
+		<div class="wp-block-columns alignwide"><!-- wp:column {"width":"80%","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"backgroundColor":"background","textColor":"foreground"} -->
+			<div class="wp-block-column has-foreground-color has-background-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small);flex-basis:80%"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+				<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1"},"spacing":{"margin":{"right":"var:preset|spacing|x-small"}}},"fontSize":"huge"} -->
+					<p class="has-huge-font-size" style="margin-right:var(--wp--preset--spacing--x-small);line-height:1">“</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"right"}} -->
+					<div class="wp-block-group"><!-- wp:paragraph -->
+						<p>Praesent quis aliquam turpis interdum potenti malesuada mattis ligula torquent condimentum iaculis mi, finibus senectus primis amet quisque in platea libero eros mus. Inceptos pretium mattis elit sociosqu dis sed primis nec lobortis erat odio ut, turpis varius euismod orci dictumst vestibulum egestas platea porttitor feugiat class. Pharetra nulla habitasse molestie nisl et nullam dictum eros habitant facilisi porta</p>
+						<!-- /wp:paragraph -->
+
+						<!-- wp:paragraph {"align":"right"} -->
+						<p class="has-text-align-right"><strong><em>-- Koji Kuno, Developer</em></strong></p>
+						<!-- /wp:paragraph --></div>
+					<!-- /wp:group --></div>
+				<!-- /wp:group --></div>
+			<!-- /wp:column -->
+
+			<!-- wp:column {"width":"20%"} -->
+			<div class="wp-block-column" style="flex-basis:20%"></div>
+			<!-- /wp:column --></div>
+		<!-- /wp:columns -->
+
+		<!-- wp:columns {"align":"wide"} -->
+		<div class="wp-block-columns alignwide"><!-- wp:column {"width":"20%"} -->
+			<div class="wp-block-column" style="flex-basis:20%"></div>
+			<!-- /wp:column -->
+
+			<!-- wp:column {"width":"80%","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"backgroundColor":"background","textColor":"foreground"} -->
+			<div class="wp-block-column has-foreground-color has-background-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small);flex-basis:80%"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+				<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1"},"spacing":{"margin":{"right":"var:preset|spacing|x-small"}}},"fontSize":"huge"} -->
+					<p class="has-huge-font-size" style="margin-right:var(--wp--preset--spacing--x-small);line-height:1">“</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"right"}} -->
+					<div class="wp-block-group"><!-- wp:paragraph -->
+						<p>Praesent quis aliquam turpis interdum potenti malesuada mattis ligula torquent condimentum iaculis mi, finibus senectus primis amet quisque in platea libero eros mus. Inceptos pretium mattis elit sociosqu dis sed primis nec lobortis erat odio ut, turpis varius euismod orci dictumst vestibulum egestas platea porttitor feugiat class. Pharetra nulla habitasse molestie nisl et nullam dictum eros habitant facilisi porta</p>
+						<!-- /wp:paragraph -->
+
+						<!-- wp:paragraph {"align":"right"} -->
+						<p class="has-text-align-right"><strong><em>-- Koji Kuno, Developer</em></strong></p>
+						<!-- /wp:paragraph --></div>
+					<!-- /wp:group --></div>
+				<!-- /wp:group --></div>
+			<!-- /wp:column --></div>
+		<!-- /wp:columns -->
+
+		<!-- wp:columns {"align":"wide"} -->
+		<div class="wp-block-columns alignwide"><!-- wp:column {"width":"80%","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"backgroundColor":"background","textColor":"foreground"} -->
+			<div class="wp-block-column has-foreground-color has-background-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small);flex-basis:80%"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+				<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1"},"spacing":{"margin":{"right":"var:preset|spacing|x-small"}}},"fontSize":"huge"} -->
+					<p class="has-huge-font-size" style="margin-right:var(--wp--preset--spacing--x-small);line-height:1">“</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"right"}} -->
+					<div class="wp-block-group"><!-- wp:paragraph -->
+						<p>Praesent quis aliquam turpis interdum potenti malesuada mattis ligula torquent condimentum iaculis mi, finibus senectus primis amet quisque in platea libero eros mus. Inceptos pretium mattis elit sociosqu dis sed primis nec lobortis erat odio ut, turpis varius euismod orci dictumst vestibulum egestas platea porttitor feugiat class. Pharetra nulla habitasse molestie nisl et nullam dictum eros habitant facilisi porta</p>
+						<!-- /wp:paragraph -->
+
+						<!-- wp:paragraph {"align":"right"} -->
+						<p class="has-text-align-right"><strong><em>-- Koji Kuno, Developer</em></strong></p>
+						<!-- /wp:paragraph --></div>
+					<!-- /wp:group --></div>
+				<!-- /wp:group --></div>
+			<!-- /wp:column -->
+
+			<!-- wp:column {"width":"20%"} -->
+			<div class="wp-block-column" style="flex-basis:20%"></div>
+			<!-- /wp:column --></div>
+		<!-- /wp:columns -->
+
+		<!-- wp:columns {"align":"wide"} -->
+		<div class="wp-block-columns alignwide"><!-- wp:column {"width":"20%"} -->
+			<div class="wp-block-column" style="flex-basis:20%"></div>
+			<!-- /wp:column -->
+
+			<!-- wp:column {"width":"80%","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","right":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small"}}},"backgroundColor":"background","textColor":"foreground"} -->
+			<div class="wp-block-column has-foreground-color has-background-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small);flex-basis:80%"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+				<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1"},"spacing":{"margin":{"right":"var:preset|spacing|x-small"}}},"fontSize":"huge"} -->
+					<p class="has-huge-font-size" style="margin-right:var(--wp--preset--spacing--x-small);line-height:1">“</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"right"}} -->
+					<div class="wp-block-group"><!-- wp:paragraph -->
+						<p>Praesent quis aliquam turpis interdum potenti malesuada mattis ligula torquent condimentum iaculis mi, finibus senectus primis amet quisque in platea libero eros mus. Inceptos pretium mattis elit sociosqu dis sed primis nec lobortis erat odio ut, turpis varius euismod orci dictumst vestibulum egestas platea porttitor feugiat class. Pharetra nulla habitasse molestie nisl et nullam dictum eros habitant facilisi porta</p>
+						<!-- /wp:paragraph -->
+
+						<!-- wp:paragraph {"align":"right"} -->
+						<p class="has-text-align-right"><strong><em>-- Koji Kuno, Developer</em></strong></p>
+						<!-- /wp:paragraph --></div>
+					<!-- /wp:group --></div>
+				<!-- /wp:group --></div>
+			<!-- /wp:column --></div>
+		<!-- /wp:columns --></div>
+	<!-- /wp:group --></div>
+<!-- /wp:group -->
