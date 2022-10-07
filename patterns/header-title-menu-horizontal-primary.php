b324298a11f2d5c81f107c326498d6f42545959b
Olein-jp <koji.kuno@gmail.com>
2022-10-07 14:32:31 +0900

    :zap: fixed bugs

--- patterns/header-title-menu-horizontal-primary.php
+++ patterns/header-title-menu-horizontal-primary.php
@@ -12,9 +12,9 @@
  * @since 1.0.0
  */
 ?>
-<!-- wp:group {"tagName":"header","align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"}}},"backgroundColor":"primary","textColor":"background","className":"site-header","layout":{"type":"default"}} -->
-<header class="wp-block-group alignfull site-header has-background-color has-primary-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-	<div class="wp-block-group"><!-- wp:site-title {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"large"} /-->
+<!-- wp:group {"tagName":"header","align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"}}},"backgroundColor":"primary","textColor":"background","className":"site-header _primary","layout":{"type":"default"}} -->
+<header class="wp-block-group alignfull site-header _primary has-background-color has-primary-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+	<div class="wp-block-group"><!-- wp:site-title {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"fontSize":"large"} /-->
 
 		<!-- wp:navigation {"ref":1929} /--></div>
 	<!-- /wp:group --></header>
