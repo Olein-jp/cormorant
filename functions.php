f9c0a04031bd769e03b5ce26cfc804d3535c8abc
Olein-jp <koji.kuno@gmail.com>
2022-10-12 16:16:16 +0900

    :boom: Deleted replacing to featured image

--- functions.php
+++ functions.php
@@ -45,55 +45,6 @@ if ( ! function_exists( 'cormorant_editor_styles' ) ) {
 	add_action( 'admin_init', 'cormorant_editor_styles' );
 }
 
-if ( ! function_exists( 'cormorant_no_feature_image_replace' ) ) {
-		/**
-		 * Output set default image when unset featured image
-		 *
-		 * @param string $block_content The block content.
-		 * @param array  $block         The full block, including name and attributes.
-		 *
-		 * @return mixed
-		 */
-	function cormorant_no_featured_image_replace( $block_content, $block ) {
-		/**
-		 * Display without single page
-		 */
-		if ( ! is_single() ) {
-			if ( 'core/post-featured-image' === $block['blockName'] && ! $block_content ) {
-				$default_images = array(
-					get_template_directory_uri() . '/assets/images/cormorant-shape-background.png',
-				);
-
-				$default_images = apply_filters( 'cormorant_add_featured_image', $default_images );
-
-				if ( count( $default_images ) === 1 ) {
-					$default_image_url = $default_images[0];
-				} else {
-					$random_image_key  = array_rand( $default_images, 1 );
-					$default_image_url = $default_images[ $random_image_key ];
-				}
-
-				$featured_image_markup = '<figure class="wp-block-post-featured-image"><img src="' . esc_url( $default_image_url ) . '" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="Default featured image" decoding="async" loading="lazy"></figure>';
-
-				if ( $block['attrs']['isLink'] ) {
-					return sprintf(
-						'<a href="%1$s">%2$s</a>',
-						get_the_permalink(),
-						$featured_image_markup
-					);
-				} else {
-					return $featured_image_markup;
-				}
-			}
-		}
-
-		return $block_content;
-	}
-
-		add_filter( 'render_block', 'cormorant_no_featured_image_replace', 10, 2 );
-}
-
-
 /**
  * Include Registration of Block Styles
  */
