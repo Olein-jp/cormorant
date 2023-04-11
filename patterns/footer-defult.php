6a7c4c35c827cec1fb2502d8e45be363ffae0559
Olein-jp <koji.kuno@gmail.com>
2023-04-11 14:51:05 +0900

    :white_check_mark: changed layout a bit

--- patterns/footer-defult.php
+++ patterns/footer-defult.php
@@ -13,11 +13,19 @@
  */
 ?>
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|s","right":"var:preset|spacing|s","bottom":"var:preset|spacing|s","left":"var:preset|spacing|s"}},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"background","layout":{"type":"default"},"fontSize":"xs"} -->
-<div class="wp-block-group alignfull has-background-color has-text-color has-link-color has-xs-font-size" style="padding-top:var(--wp--preset--spacing--s);padding-right:var(--wp--preset--spacing--s);padding-bottom:var(--wp--preset--spacing--s);padding-left:var(--wp--preset--spacing--s)"><!-- wp:group {"style":{"spacing":{"blockGap":"3px"}},"layout":{"type":"flex","flexWrap":"nowrap","orientation":"horizontal"}} -->
+<div class="wp-block-group alignfull has-background-color has-text-color has-link-color has-xs-font-size" style="padding-top:var(--wp--preset--spacing--s);padding-right:var(--wp--preset--spacing--s);padding-bottom:var(--wp--preset--spacing--s);padding-left:var(--wp--preset--spacing--s)"><!-- wp:group {"style":{"spacing":{"blockGap":"3px"}},"layout":{"type":"flex","flexWrap":"nowrap","orientation":"horizontal","justifyContent":"center"}} -->
 	<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"xs"} -->
-		<p class="has-xs-font-size">©️</p>
+		<p class="has-xs-font-size">Copyright</p>
 		<!-- /wp:paragraph -->
 
-		<!-- wp:site-title {"level":0,"isLink":false,"fontSize":"xs"} /--></div>
+		<!-- wp:site-title {"level":0,"isLink":false,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"xs"} /-->
+
+		<!-- wp:paragraph {"fontSize":"xs"} -->
+		<p class="has-xs-font-size">. </p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:paragraph {"fontSize":"xs"} -->
+		<p class="has-xs-font-size">Powered by <a href="https://wordpress.org">WordPress</a>.</p>
+		<!-- /wp:paragraph --></div>
 	<!-- /wp:group --></div>
 <!-- /wp:group -->
--- patterns/header-default.php
+++ patterns/header-default.php
@@ -12,8 +12,10 @@
  * @since 1.0.0
  */
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|s","right":"var:preset|spacing|s","bottom":"var:preset|spacing|s","left":"var:preset|spacing|s"}}},"backgroundColor":"primary","textColor":"background","className":"site-header _primary","layout":{"type":"default"}} -->
-<div class="wp-block-group alignfull site-header _primary has-background-color has-primary-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--s);padding-right:var(--wp--preset--spacing--s);padding-bottom:var(--wp--preset--spacing--s);padding-left:var(--wp--preset--spacing--s)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-	<div class="wp-block-group"><!-- wp:site-title {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"fontSize":"s"} /--></div>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|s","right":"var:preset|spacing|s","bottom":"var:preset|spacing|s","left":"var:preset|spacing|s"}}},"textColor":"background","className":"site-header _primary","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull site-header _primary has-background-color has-text-color" style="padding-top:var(--wp--preset--spacing--s);padding-right:var(--wp--preset--spacing--s);padding-bottom:var(--wp--preset--spacing--s);padding-left:var(--wp--preset--spacing--s)"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|s","right":"var:preset|spacing|s","bottom":"var:preset|spacing|s","left":"var:preset|spacing|s"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+	<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--s);padding-right:var(--wp--preset--spacing--s);padding-bottom:var(--wp--preset--spacing--s);padding-left:var(--wp--preset--spacing--s)"><!-- wp:site-title {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary","fontSize":"m"} /-->
+
+		<!-- wp:navigation {"ref":2651} /--></div>
 	<!-- /wp:group --></div>
 <!-- /wp:group -->
