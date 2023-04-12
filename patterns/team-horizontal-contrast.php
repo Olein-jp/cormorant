e4f424673667e8d13628533723000999f5a4e30b
Olein-jp <koji.kuno@gmail.com>
2023-04-12 14:08:50 +0900

    :white_check_mark: updated pattern of team

---
+++ patterns/team-horizontal-contrast.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Title: Team horizontal Contrast
+ * Slug: cormorant/team-horizontal-contrast
+ * Categories: cormorant-team
+ * Description: Team horizontal colored contrast color
+ * Keywords: cormorant
+ * Block Types: core/group, core/columns, core/paragraph, core/buttons, core/heading, core/image
+ *
+ * @author Koji Kuno
+ * @package cormorant
+ * @since 1.0.0
+ */
+?>
+<!-- wp:group {"align":"full","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|4-xl","bottom":"var:preset|spacing|4-xl"}}},"backgroundColor":"base","textColor":"contrast","layout":{"type":"constrained"}} -->
+	<div class="wp-block-group alignwide has-contrast-color has-base-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--4-xl);padding-bottom:var(--wp--preset--spacing--4-xl)"><!-- wp:group {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|xl"}}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--xl)"><!-- wp:heading {"textAlign":"center","fontSize":"3-xl"} -->
+			<h2 class="wp-block-heading has-text-align-center has-3-xl-font-size">We are the Team</h2>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph {"align":"center","fontSize":"xs"} -->
+			<p class="has-text-align-center has-xs-font-size">Potenti netus aptent tempor ornare penatibus purus</p>
+			<!-- /wp:paragraph --></div>
+		<!-- /wp:group -->
+
+		<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|xl","left":"var:preset|spacing|xl"}}}} -->
+		<div class="wp-block-columns alignwide"><!-- wp:column -->
+			<div class="wp-block-column"><!-- wp:image {"id":2194,"sizeSlug":"large","linkDestination":"none"} -->
+				<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/cormorant-shape-white.png'; ?>" alt="<?php echo esc_attr__( 'Sample Image', 'cormorant' ); ?>" class="wp-image-2194"/></figure>
+				<!-- /wp:image -->
+
+				<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"l"} -->
+				<h3 class="wp-block-heading has-text-align-center has-l-font-size">Member Name</h3>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph {"fontSize":"xs"} -->
+				<p class="has-xs-font-size">Ad accumsan est turpis elit maximus senectus tincidunt ultricies at ante habitant risus netus, justo curae montes vitae quisque tristique felis adipiscing nibh aptent efficitur.</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:buttons -->
+				<div class="wp-block-buttons"><!-- wp:button {"width":100,"className":"is-style-cormorant-filled-square-button-contrast"} -->
+					<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-cormorant-filled-square-button-contrast"><a class="wp-block-button__link wp-element-button">View Profile</a></div>
+					<!-- /wp:button --></div>
+				<!-- /wp:buttons --></div>
+			<!-- /wp:column -->
+
+			<!-- wp:column -->
+			<div class="wp-block-column"><!-- wp:image {"id":2194,"sizeSlug":"large","linkDestination":"none"} -->
+				<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/cormorant-shape-white.png'; ?>" alt="<?php echo esc_attr__( 'Sample Image', 'cormorant' ); ?>" class="wp-image-2194"/></figure>
+				<!-- /wp:image -->
+
+				<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"l"} -->
+				<h3 class="wp-block-heading has-text-align-center has-l-font-size">Member Name</h3>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph {"fontSize":"xs"} -->
+				<p class="has-xs-font-size">Ad accumsan est turpis elit maximus senectus tincidunt ultricies at ante habitant risus netus, justo curae montes vitae quisque tristique felis adipiscing nibh aptent efficitur.</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:buttons -->
+				<div class="wp-block-buttons"><!-- wp:button {"width":100,"className":"is-style-cormorant-filled-square-button-contrast"} -->
+					<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-cormorant-filled-square-button-contrast"><a class="wp-block-button__link wp-element-button">View Profile</a></div>
+					<!-- /wp:button --></div>
+				<!-- /wp:buttons --></div>
+			<!-- /wp:column -->
+
+			<!-- wp:column -->
+			<div class="wp-block-column"><!-- wp:image {"id":2194,"sizeSlug":"large","linkDestination":"none"} -->
+				<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/cormorant-shape-white.png'; ?>" alt="<?php echo esc_attr__( 'Sample Image', 'cormorant' ); ?>" class="wp-image-2194"/></figure>
+				<!-- /wp:image -->
+
+				<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"l"} -->
+				<h3 class="wp-block-heading has-text-align-center has-l-font-size">Member Name</h3>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph {"fontSize":"xs"} -->
+				<p class="has-xs-font-size">Ad accumsan est turpis elit maximus senectus tincidunt ultricies at ante habitant risus netus, justo curae montes vitae quisque tristique felis adipiscing nibh aptent efficitur.</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:buttons -->
+				<div class="wp-block-buttons"><!-- wp:button {"width":100,"className":"is-style-cormorant-filled-square-button-contrast"} -->
+					<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-cormorant-filled-square-button-contrast"><a class="wp-block-button__link wp-element-button">View Profile</a></div>
+					<!-- /wp:button --></div>
+				<!-- /wp:buttons --></div>
+			<!-- /wp:column -->
+
+			<!-- wp:column -->
+			<div class="wp-block-column"><!-- wp:image {"id":2194,"sizeSlug":"large","linkDestination":"none"} -->
+				<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/cormorant-shape-white.png'; ?>" alt="<?php echo esc_attr__( 'Sample Image', 'cormorant' ); ?>" class="wp-image-2194"/></figure>
+				<!-- /wp:image -->
+
+				<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"l"} -->
+				<h3 class="wp-block-heading has-text-align-center has-l-font-size">Member Name</h3>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph {"fontSize":"xs"} -->
+				<p class="has-xs-font-size">Ad accumsan est turpis elit maximus senectus tincidunt ultricies at ante habitant risus netus, justo curae montes vitae quisque tristique felis adipiscing nibh aptent efficitur.</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:buttons -->
+				<div class="wp-block-buttons"><!-- wp:button {"width":100,"className":"is-style-cormorant-filled-square-button-contrast"} -->
+					<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-cormorant-filled-square-button-contrast"><a class="wp-block-button__link wp-element-button">View Profile</a></div>
+					<!-- /wp:button --></div>
+				<!-- /wp:buttons --></div>
+			<!-- /wp:column --></div>
+		<!-- /wp:columns --></div>
+	<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/team-vertical-base.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Title: Team vertical Base
+ * Slug: cormorant/team-vertical-base
+ * Categories: cormorant-team
+ * Description: Team vertical colored base color
+ * Keywords: cormorant
+ * Block Types: core/group, core/columns, core/paragraph, core/buttons, core/heading, core/image
+ *
+ * @author Koji Kuno
+ * @package cormorant
+ * @since 1.0.0
+ */
+?>
+<!-- wp:group {"align":"full","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|4-xl","bottom":"var:preset|spacing|4-xl"}}},"textColor":"base","layout":{"type":"constrained"}} -->
+	<div class="wp-block-group alignwide has-base-color has-text-color" style="padding-top:var(--wp--preset--spacing--4-xl);padding-bottom:var(--wp--preset--spacing--4-xl)"><!-- wp:group {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|xl"}}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--xl)"><!-- wp:heading {"textAlign":"center","fontSize":"3-xl"} -->
+			<h2 class="wp-block-heading has-text-align-center has-3-xl-font-size">We are the Team</h2>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph {"align":"center","fontSize":"xs"} -->
+			<p class="has-text-align-center has-xs-font-size">Potenti netus aptent tempor ornare penatibus purus</p>
+			<!-- /wp:paragraph --></div>
+		<!-- /wp:group -->
+
+		<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xl"}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group"><!-- wp:columns {"verticalAlignment":"center"} -->
+			<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"33.33%"} -->
+				<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.33%"><!-- wp:image {"id":2193,"sizeSlug":"large","linkDestination":"none"} -->
+					<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/cormorant-shape-black.png'; ?>" alt="<?php echo esc_attr__( 'Sample Image', 'cormorant' ); ?>" class="wp-image-2193"/></figure>
+					<!-- /wp:image --></div>
+				<!-- /wp:column -->
+
+				<!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
+				<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%"><!-- wp:heading {"textAlign":"left","level":3,"fontSize":"l"} -->
+					<h3 class="wp-block-heading has-text-align-left has-l-font-size">Member Name</h3>
+					<!-- /wp:heading -->
+
+					<!-- wp:paragraph {"fontSize":"xs"} -->
+					<p class="has-xs-font-size">Ad accumsan est turpis elit maximus senectus tincidunt ultricies at ante habitant risus netus, justo curae montes vitae quisque tristique felis adipiscing nibh aptent efficitur.</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
+					<div class="wp-block-buttons"><!-- wp:button {"width":50,"className":"is-style-cormorant-filled-square-button-base"} -->
+						<div class="wp-block-button has-custom-width wp-block-button__width-50 is-style-cormorant-filled-square-button-base"><a class="wp-block-button__link wp-element-button">View Profile</a></div>
+						<!-- /wp:button --></div>
+					<!-- /wp:buttons --></div>
+				<!-- /wp:column --></div>
+			<!-- /wp:columns -->
+
+			<!-- wp:columns {"verticalAlignment":"center"} -->
+			<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"33.33%"} -->
+				<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.33%"><!-- wp:image {"id":2193,"sizeSlug":"large","linkDestination":"none"} -->
+					<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/cormorant-shape-black.png'; ?>" alt="<?php echo esc_attr__( 'Sample Image', 'cormorant' ); ?>" class="wp-image-2193"/></figure>
+					<!-- /wp:image --></div>
+				<!-- /wp:column -->
+
+				<!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
+				<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%"><!-- wp:heading {"textAlign":"left","level":3,"fontSize":"l"} -->
+					<h3 class="wp-block-heading has-text-align-left has-l-font-size">Member Name</h3>
+					<!-- /wp:heading -->
+
+					<!-- wp:paragraph {"fontSize":"xs"} -->
+					<p class="has-xs-font-size">Ad accumsan est turpis elit maximus senectus tincidunt ultricies at ante habitant risus netus, justo curae montes vitae quisque tristique felis adipiscing nibh aptent efficitur.</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
+					<div class="wp-block-buttons"><!-- wp:button {"width":50,"className":"is-style-cormorant-filled-square-button-base"} -->
+						<div class="wp-block-button has-custom-width wp-block-button__width-50 is-style-cormorant-filled-square-button-base"><a class="wp-block-button__link wp-element-button">View Profile</a></div>
+						<!-- /wp:button --></div>
+					<!-- /wp:buttons --></div>
+				<!-- /wp:column --></div>
+			<!-- /wp:columns -->
+
+			<!-- wp:columns {"verticalAlignment":"center"} -->
+			<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"33.33%"} -->
+				<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.33%"><!-- wp:image {"id":2193,"sizeSlug":"large","linkDestination":"none"} -->
+					<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/cormorant-shape-black.png'; ?>" alt="<?php echo esc_attr__( 'Sample Image', 'cormorant' ); ?>" class="wp-image-2193"/></figure>
+					<!-- /wp:image --></div>
+				<!-- /wp:column -->
+
+				<!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
+				<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%"><!-- wp:heading {"textAlign":"left","level":3,"fontSize":"l"} -->
+					<h3 class="wp-block-heading has-text-align-left has-l-font-size">Member Name</h3>
+					<!-- /wp:heading -->
+
+					<!-- wp:paragraph {"fontSize":"xs"} -->
+					<p class="has-xs-font-size">Ad accumsan est turpis elit maximus senectus tincidunt ultricies at ante habitant risus netus, justo curae montes vitae quisque tristique felis adipiscing nibh aptent efficitur.</p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
+					<div class="wp-block-buttons"><!-- wp:button {"width":50,"className":"is-style-cormorant-filled-square-button-base"} -->
+						<div class="wp-block-button has-custom-width wp-block-button__width-50 is-style-cormorant-filled-square-button-base"><a class="wp-block-button__link wp-element-button">View Profile</a></div>
+						<!-- /wp:button --></div>
+					<!-- /wp:buttons --></div>
+				<!-- /wp:column --></div>
+			<!-- /wp:columns --></div>
+		<!-- /wp:group --></div>
+	<!-- /wp:group --></div>
+<!-- /wp:group -->
