718458568fe0b135c0440c702ef57d8deb7ba5a0
Olein-jp <koji.kuno@gmail.com>
2023-08-01 14:13:03 +0900

    fixed patterns

--- patterns/cta-horizontal-contrast.php
+++ patterns/cta-horizontal-contrast.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: CTA Horizontal Contrast
- * Slug: cormorant/cta-horizontal-contrast
+ * Slug: cormorant/cta/horizontal-contrast
  * Categories: cormorant-cta
  * Description: Call To Action layout Horizontal colored contrast color
  * Keywords: cormorant
@@ -12,19 +12,19 @@
  * @since 1.0.0
  */
 ?>
-<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0"}},"textColor":"background","layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide has-background-color has-text-color"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","right":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl","left":"var:preset|spacing|xl"}},"border":{"width":"0px","style":"none"},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"backgroundColor":"base","textColor":"contrast","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-	<div class="wp-block-group alignwide has-contrast-color has-base-background-color has-text-color has-background has-link-color" style="border-style:none;border-width:0px;padding-top:var(--wp--preset--spacing--xl);padding-right:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl);padding-left:var(--wp--preset--spacing--xl)"><!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"},"padding":{"top":"0"}}}} -->
+<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"0"}},"textColor":"background","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide has-background-color has-text-color"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","right":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl","left":"var:preset|spacing|xl"}},"border":{"width":"0px","style":"none"}},"backgroundColor":"base","textColor":"contrast","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+	<div class="wp-block-group has-contrast-color has-base-background-color has-text-color has-background" style="border-style:none;border-width:0px;padding-top:var(--wp--preset--spacing--xl);padding-right:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl);padding-left:var(--wp--preset--spacing--xl)"><!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|x-small"},"padding":{"top":"0"}}}} -->
 		<div class="wp-block-columns are-vertically-aligned-center" style="margin-top:var(--wp--preset--spacing--x-small);padding-top:0"><!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
-			<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%"><!-- wp:paragraph {"fontSize":"xs"} -->
-				<p class="has-xs-font-size">Diam elit magnis faucibus arcu duis pulvinar malesuada placerat aenean cubilia purus risus sem, luctus in dictum himenaeos urna id </p>
+			<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%"><!-- wp:paragraph {"fontSize":"s"} -->
+				<p class="has-s-font-size"><?php esc_html_e( 'New opportunities are waiting for you! If you have any questions or queries, please do not hesitate to let us know. Our expert team will be happy to answer your questions in detail.', 'cormorant' ); ?></p>
 				<!-- /wp:paragraph --></div>
 			<!-- /wp:column -->
 
 			<!-- wp:column {"verticalAlignment":"center","width":"33.33%"} -->
 			<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.33%"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"blockGap":"0"}},"fontSize":"medium"} -->
-				<div class="wp-block-buttons has-custom-font-size has-medium-font-size"><!-- wp:button {"backgroundColor":"background","className":"is-style-cormorant-background-outline-square-button is-style-cormorant-filled-square-button-contrast","fontSize":"xs"} -->
-					<div class="wp-block-button has-custom-font-size is-style-cormorant-background-outline-square-button is-style-cormorant-filled-square-button-contrast has-xs-font-size"><a class="wp-block-button__link has-background-background-color has-background wp-element-button">Contact us</a></div>
+				<div class="wp-block-buttons has-custom-font-size has-medium-font-size"><!-- wp:button {"backgroundColor":"background","className":"is-style-cormorant-filled-square-button-contrast","fontSize":"s"} -->
+					<div class="wp-block-button has-custom-font-size is-style-cormorant-filled-square-button-contrast has-s-font-size"><a class="wp-block-button__link has-background-background-color has-background wp-element-button" href="#"><strong><?php esc_html_e( 'Contact us', 'cormorant' ); ?></strong></a></div>
 					<!-- /wp:button --></div>
 				<!-- /wp:buttons --></div>
 			<!-- /wp:column --></div>
--- patterns/cta-vertical-base.php
+++ patterns/cta-vertical-base.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: CTA Vertical Base
- * Slug: cormorant/cta-vertical-base
+ * Slug: cormorant/cta/vertical-base
  * Categories: cormorant-cta
  * Description: Call To Action layout Vertical colored base color
  * Keywords: cormorant
@@ -15,16 +15,16 @@
 <!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"0"}},"textColor":"background","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull has-background-color has-text-color"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50","padding":{"top":"var:preset|spacing|xl","right":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl","left":"var:preset|spacing|xl"}},"border":{"width":"1px"}},"borderColor":"base","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
 	<div class="wp-block-group has-border-color has-base-border-color" style="border-width:1px;padding-top:var(--wp--preset--spacing--xl);padding-right:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl);padding-left:var(--wp--preset--spacing--xl)"><!-- wp:heading {"level":3} -->
-		<h3 class="wp-block-heading"><strong>Contact us</strong></h3>
+		<h3 class="wp-block-heading"><strong><?php esc_html_e( 'Thank you for your interest!', 'cormorant' ); ?></strong></h3>
 		<!-- /wp:heading -->
 
 		<!-- wp:paragraph -->
-		<p>Diam elit magnis faucibus arcu duis pulvinar malesuada placerat aenean cubilia purus risus sem, luctus in dictum himenaeos urna id commodo sollicitudin iaculis cursus per. Habitant tortor non nascetur lacus enim rutrum fringilla vivamus integer litora vitae nostra</p>
+		<p><?php esc_html_e( 'New opportunities are waiting for you! If you have any questions or queries, please do not hesitate to let us know. Our expert team will be happy to answer your questions in detail.', 'cormorant' ); ?></p>
 		<!-- /wp:paragraph -->
 
 		<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"blockGap":"0"}},"fontSize":"medium"} -->
-		<div class="wp-block-buttons has-custom-font-size has-medium-font-size"><!-- wp:button {"backgroundColor":"background","className":"is-style-cormorant-background-outline-square-button is-style-cormorant-filled-square-button-base","fontSize":"xs"} -->
-			<div class="wp-block-button has-custom-font-size is-style-cormorant-background-outline-square-button is-style-cormorant-filled-square-button-base has-xs-font-size"><a class="wp-block-button__link has-background-background-color has-background wp-element-button">Contact Us</a></div>
+		<div class="wp-block-buttons has-custom-font-size has-medium-font-size"><!-- wp:button {"backgroundColor":"background","className":"is-style-cormorant-background-outline-square-button is-style-cormorant-filled-square-button-base","fontSize":"s"} -->
+			<div class="wp-block-button has-custom-font-size is-style-cormorant-background-outline-square-button is-style-cormorant-filled-square-button-base has-s-font-size"><a class="wp-block-button__link has-background-background-color has-background wp-element-button"><strong><?php esc_html_e( 'Contact Us', 'cormorant' ); ?></strong></a></div>
 			<!-- /wp:button --></div>
 		<!-- /wp:buttons --></div>
 	<!-- /wp:group --></div>
