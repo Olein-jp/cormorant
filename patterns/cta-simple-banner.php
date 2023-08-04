10bbba6e2e36f6725286bd382cdf55bf6446fb27
Olein-jp <koji.kuno@gmail.com>
2023-08-04 20:17:47 +0900

    changed cta block patterns

---
+++ patterns/cta-simple-banner.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Title: CTA Simple Banner
+ * Slug: cormorant/cta/simple-banner
+ * Categories: cormorant-cta
+ * Description: Just simple banner
+ * Keywords: cormorant
+ * Block Types: core/group, core/paragraph, core/buttons,
+ *
+ * @author Koji Kuno
+ * @package cormorant
+ * @since 3.0.0
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl"}},"border":{"radius":"0px"}},"textColor":"contrast","gradient":"tertiary-to-primary","layout":{"type":"constrained"}} -->
+	<div class="wp-block-group alignwide has-contrast-color has-tertiary-to-primary-gradient-background has-text-color has-background" style="border-radius:0px;padding-top:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|m"}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","fontSize":"4-xl"} -->
+			<h2 class="wp-block-heading has-text-align-center has-4-xl-font-size">Need more design?</h2>
+			<!-- /wp:heading -->
+
+			<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+			<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-cormorant-filled-square-button-secondary"} -->
+				<div class="wp-block-button is-style-cormorant-filled-square-button-secondary"><a class="wp-block-button__link wp-element-button" href="#"><strong>Chat with us</strong></a></div>
+				<!-- /wp:button --></div>
+			<!-- /wp:buttons --></div>
+		<!-- /wp:group --></div>
+	<!-- /wp:group --></div>
+<!-- /wp:group -->
