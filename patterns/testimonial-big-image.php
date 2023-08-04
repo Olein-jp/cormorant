ef86d7b6b92bd309580a42b6b0a72bfe78e8a02e
Olein-jp <koji.kuno@gmail.com>
2023-08-04 20:18:38 +0900

    created testimonial block patterns

---
+++ patterns/testimonial-big-image.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Title: Big Image Testimonial
+ * Slug: cormorant/testimonial/big-image
+ * Categories: cormorant-testimonial
+ * Description: Testimonial with big image
+ * Keywords: cormorant
+ * Block Types: core/group, core/columns, core/paragraph, core/paragraph
+ *
+ * @author Koji Kuno
+ * @package cormorant
+ * @since 3.0.0
+ */
+?>
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:columns {"verticalAlignment":null,"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":{"top":"0","left":"0"}}}} -->
+	<div class="wp-block-columns" style="margin-top:0;margin-bottom:0"><!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:image {"id":3219,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/cormorant-sample-12.jpg' ); ?>" alt="" class="wp-image-3219" style="aspect-ratio:1;object-fit:cover"/></figure>
+			<!-- /wp:image --></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"verticalAlignment":"center","width":"","style":{"spacing":{"blockGap":"var:preset|spacing|s","padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl","left":"0","right":"0"}}}} -->
+		<div class="wp-block-column is-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--xl);padding-right:0;padding-bottom:var(--wp--preset--spacing--xl);padding-left:0"><!-- wp:paragraph {"align":"center","style":{"spacing":{"padding":{"right":"var:preset|spacing|xl","left":"var:preset|spacing|xl"}}},"fontSize":"l"} -->
+			<p class="has-text-align-center has-l-font-size" style="padding-right:var(--wp--preset--spacing--xl);padding-left:var(--wp--preset--spacing--xl)"><strong><?php echo esc_attr( 'Since using our product, our business results have improved dramatically! Efficient task management and real-time data analysis capabilities have supported our decision-making and helped us develop strategies to increase sales.', 'cormorant' ); ?></strong></p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"right":"var:preset|spacing|xl","left":"var:preset|spacing|xl"}}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--xl);padding-left:var(--wp--preset--spacing--xl)"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"12px"}}} -->
+				<p class="has-text-align-center" style="font-size:12px"><?php echo esc_attr( 'Marketing Director', 'cormorant' ); ?></p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph {"align":"center","fontSize":"m"} -->
+				<p class="has-text-align-center has-m-font-size"><strong><?php echo esc_attr( 'Matthias Klaus', 'cormorant' ); ?></strong></p>
+				<!-- /wp:paragraph --></div>
+			<!-- /wp:group --></div>
+		<!-- /wp:column --></div>
+	<!-- /wp:columns -->
+
+	<!-- wp:columns {"verticalAlignment":null,"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":{"top":"0","left":"0"}}},"className":"u-reverse-2-column"} -->
+	<div class="wp-block-columns u-reverse-2-column" style="margin-top:0;margin-bottom:0"><!-- wp:column {"verticalAlignment":"center","width":"","style":{"spacing":{"blockGap":"var:preset|spacing|s","padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl","left":"0","right":"0"}}}} -->
+		<div class="wp-block-column is-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--xl);padding-right:0;padding-bottom:var(--wp--preset--spacing--xl);padding-left:0"><!-- wp:paragraph {"align":"center","style":{"spacing":{"padding":{"right":"var:preset|spacing|xl","left":"var:preset|spacing|xl"}}},"fontSize":"l"} -->
+			<p class="has-text-align-center has-l-font-size" style="padding-right:var(--wp--preset--spacing--xl);padding-left:var(--wp--preset--spacing--xl)"><strong><?php echo esc_attr( 'For many years we have been using other companies products, but since we switched to yours, it has made a real difference to our business. We have been particularly surprised by the robustness and customisability of your product. We have been able to add custom functionality to suit our particular needs, which has enabled us to streamline our operations.', 'cormorant' ); ?></strong></p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"right":"var:preset|spacing|xl","left":"var:preset|spacing|xl"}}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--xl);padding-left:var(--wp--preset--spacing--xl)"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"12px"}}} -->
+				<p class="has-text-align-center" style="font-size:12px"><?php echo esc_attr( 'Chef Designer', 'cormorant' ); ?></p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph {"align":"center","fontSize":"m"} -->
+				<p class="has-text-align-center has-m-font-size"><strong><?php echo esc_attr( 'Natalia Andrea', 'cormorant' ); ?></strong></p>
+				<!-- /wp:paragraph --></div>
+			<!-- /wp:group --></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:image {"id":3218,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/cormorant-sample-11.jpg' ); ?>" alt="" class="wp-image-3218" style="aspect-ratio:1;object-fit:cover"/></figure>
+			<!-- /wp:image --></div>
+		<!-- /wp:column --></div>
+	<!-- /wp:columns --></div>
+<!-- /wp:group -->
---
+++ patterns/testimonial-picture-on-card.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Title: Picture on Card Testimonial
+ * Slug: cormorant/testimonial/picture-on-card
+ * Categories: cormorant-testimonial
+ * Description: Picture on card testimonial
+ * Keywords: cormorant
+ * Block Types: core/group, core/columns, core/paragraph, core/paragraph
+ *
+ * @author Koji Kuno
+ * @package cormorant
+ * @since 3.0.0
+ */
+?>
+<!-- wp:group {"tagName":"section","style":{"spacing":{"padding":{"top":"var:preset|spacing|3-xl","bottom":"var:preset|spacing|3-xl"}},"color":{"gradient":"linear-gradient(150deg,rgb(238,238,238) 46%,rgb(255,255,255) 46%)"}},"layout":{"type":"default"}} -->
+<section class="wp-block-group has-background" style="background:linear-gradient(150deg,rgb(238,238,238) 46%,rgb(255,255,255) 46%);padding-top:var(--wp--preset--spacing--3-xl);padding-bottom:var(--wp--preset--spacing--3-xl)"><!-- wp:group {"tagName":"header","style":{"spacing":{"blockGap":"var:preset|spacing|xs","margin":{"bottom":"var:preset|spacing|xl"}}},"layout":{"type":"constrained"}} -->
+	<header class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--xl)"><!-- wp:heading {"textAlign":"center","fontSize":"3-xl"} -->
+		<h2 class="wp-block-heading has-text-align-center has-3-xl-font-size"><?php echo esc_attr( 'Testimonial', 'cormorant' ); ?></h2>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph {"align":"center"} -->
+		<p class="has-text-align-center"><?php echo esc_attr( 'We bring you real feedback from our customers.', 'cormorant' ); ?></p>
+		<!-- /wp:paragraph --></header>
+	<!-- /wp:group -->
+
+	<!-- wp:group {"layout":{"type":"default"}} -->
+	<div class="wp-block-group"><!-- wp:columns -->
+		<div class="wp-block-columns"><!-- wp:column -->
+			<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|m","padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl","left":"var:preset|spacing|m","right":"var:preset|spacing|m"}}},"backgroundColor":"contrast","textColor":"base","className":"is-style-cormorant-blur-shadow-group","layout":{"type":"constrained"}} -->
+				<div class="wp-block-group is-style-cormorant-blur-shadow-group has-base-color has-contrast-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--xl);padding-right:var(--wp--preset--spacing--m);padding-bottom:var(--wp--preset--spacing--xl);padding-left:var(--wp--preset--spacing--m)"><!-- wp:group {"layout":{"type":"constrained"}} -->
+					<div class="wp-block-group"><!-- wp:image {"align":"center","id":3219,"width":120,"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"100px","width":"3px"}},"borderColor":"lighter-gray","className":"is-style-cormorant-blur-shadow-image"} -->
+						<figure class="wp-block-image aligncenter size-full is-resized has-custom-border is-style-cormorant-blur-shadow-image"><img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/cormorant-sample-12.jpg' ); ?>" alt="" class="has-border-color has-lighter-gray-border-color wp-image-3219" style="border-width:3px;border-radius:100px;width:120px" width="120"/></figure>
+						<!-- /wp:image --></div>
+					<!-- /wp:group -->
+
+					<!-- wp:paragraph {"textColor":"dark-gray"} -->
+					<p class="has-dark-gray-color has-text-color"><?php echo esc_attr( 'Since using our product, our business results have improved dramatically! Efficient task management and real-time data analysis capabilities have supported our decision-making and helped us develop strategies to increase sales. Furthermore, we are very grateful for the excellent customer support. Their professionalism and immediate response has always given us peace of mind.', 'cormorant' ); ?></p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+					<div class="wp-block-group"><!-- wp:paragraph {"align":"center","textColor":"light-gray","fontSize":"xs"} -->
+						<p class="has-text-align-center has-light-gray-color has-text-color has-xs-font-size"><strong><?php echo esc_attr( 'Marketing Director', 'cormorant' ); ?></strong></p>
+						<!-- /wp:paragraph -->
+
+						<!-- wp:paragraph {"align":"center","fontSize":"m"} -->
+						<p class="has-text-align-center has-m-font-size"><strong><?php echo esc_attr( 'Matthias Klaus', 'cormorant' ); ?></strong></p>
+						<!-- /wp:paragraph --></div>
+					<!-- /wp:group --></div>
+				<!-- /wp:group --></div>
+			<!-- /wp:column -->
+
+			<!-- wp:column -->
+			<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|m","padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl","left":"var:preset|spacing|m","right":"var:preset|spacing|m"}}},"backgroundColor":"contrast","textColor":"base","className":"is-style-cormorant-blur-shadow-group","layout":{"type":"constrained"}} -->
+				<div class="wp-block-group is-style-cormorant-blur-shadow-group has-base-color has-contrast-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--xl);padding-right:var(--wp--preset--spacing--m);padding-bottom:var(--wp--preset--spacing--xl);padding-left:var(--wp--preset--spacing--m)"><!-- wp:group {"layout":{"type":"constrained"}} -->
+					<div class="wp-block-group"><!-- wp:image {"align":"center","id":3218,"width":120,"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"100px","width":"3px"}},"borderColor":"lighter-gray","className":"is-style-cormorant-blur-shadow-image"} -->
+						<figure class="wp-block-image aligncenter size-full is-resized has-custom-border is-style-cormorant-blur-shadow-image"><img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/cormorant-sample-11.jpg' ); ?>" alt="" class="has-border-color has-lighter-gray-border-color wp-image-3218" style="border-width:3px;border-radius:100px;width:120px" width="120"/></figure>
+						<!-- /wp:image --></div>
+					<!-- /wp:group -->
+
+					<!-- wp:paragraph {"textColor":"dark-gray"} -->
+					<p class="has-dark-gray-color has-text-color"><?php echo esc_attr( 'The new products we have introduced have made a significant contribution to the efficiency of our business. In particular, the automation of tasks and real-time data visualisation were excellent features. This has streamlined our internal processes and increased the productivity of our entire staff. Furthermore, we were amazed by the ease of use. We were able to operate it straight away without the need for training.', 'cormorant' ); ?></p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+					<div class="wp-block-group"><!-- wp:paragraph {"align":"center","textColor":"light-gray","fontSize":"xs"} -->
+						<p class="has-text-align-center has-light-gray-color has-text-color has-xs-font-size"><strong><?php echo esc_attr( 'Chief Designer', 'cormorant' ); ?></strong></p>
+						<!-- /wp:paragraph -->
+
+						<!-- wp:paragraph {"align":"center","fontSize":"m"} -->
+						<p class="has-text-align-center has-m-font-size"><strong><?php echo esc_attr( 'Natalia Andrea', 'cormorant' ); ?></strong></p>
+						<!-- /wp:paragraph --></div>
+					<!-- /wp:group --></div>
+				<!-- /wp:group --></div>
+			<!-- /wp:column -->
+
+			<!-- wp:column -->
+			<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|m","padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl","left":"var:preset|spacing|m","right":"var:preset|spacing|m"}}},"backgroundColor":"contrast","textColor":"base","className":"is-style-cormorant-blur-shadow-group","layout":{"type":"constrained"}} -->
+				<div class="wp-block-group is-style-cormorant-blur-shadow-group has-base-color has-contrast-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--xl);padding-right:var(--wp--preset--spacing--m);padding-bottom:var(--wp--preset--spacing--xl);padding-left:var(--wp--preset--spacing--m)"><!-- wp:group {"layout":{"type":"constrained"}} -->
+					<div class="wp-block-group"><!-- wp:image {"align":"center","id":3206,"width":120,"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"100px","width":"3px"}},"borderColor":"lighter-gray","className":"is-style-cormorant-blur-shadow-image"} -->
+						<figure class="wp-block-image aligncenter size-full is-resized has-custom-border is-style-cormorant-blur-shadow-image"><img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/cormorant-sample-10.jpg' ); ?>" alt="WordPress 100%GPL Block Theme Cormorant Sample Image 10" class="has-border-color has-lighter-gray-border-color wp-image-3206" style="border-width:3px;border-radius:100px;width:120px" width="120"/></figure>
+						<!-- /wp:image --></div>
+					<!-- /wp:group -->
+
+					<!-- wp:paragraph {"textColor":"dark-gray"} -->
+					<p class="has-dark-gray-color has-text-color"><?php echo esc_attr( 'For many years we have been using other companies products, but since we switched to yours, it has made a real difference to our business. We have been particularly surprised by the robustness and customisability of your product. We have been able to add custom functionality to suit our particular needs, which has enabled us to streamline our operations.', 'cormorant' ); ?></p>
+					<!-- /wp:paragraph -->
+
+					<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+					<div class="wp-block-group"><!-- wp:paragraph {"align":"center","textColor":"light-gray","fontSize":"xs"} -->
+						<p class="has-text-align-center has-light-gray-color has-text-color has-xs-font-size"><strong><?php echo esc_attr( 'Project Manager', 'cormorant' ); ?></strong></p>
+						<!-- /wp:paragraph -->
+
+						<!-- wp:paragraph {"align":"center","fontSize":"m"} -->
+						<p class="has-text-align-center has-m-font-size"><strong><?php echo esc_attr( 'Emma Johansson', 'cormorant' ); ?></strong></p>
+						<!-- /wp:paragraph --></div>
+					<!-- /wp:group --></div>
+				<!-- /wp:group --></div>
+			<!-- /wp:column --></div>
+		<!-- /wp:columns --></div>
+	<!-- /wp:group --></section>
+<!-- /wp:group -->
