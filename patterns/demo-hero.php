c14ad944fc37d944c4c71d50ae7acaf4c4c36cca
Olein-jp <koji.kuno@gmail.com>
2023-08-07 19:49:25 +0900

    updated pattern

--- patterns/demo-hero.php
+++ patterns/demo-hero.php
@@ -12,22 +12,30 @@
  * @since 3.0.0
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|3-xl","right":"var:preset|spacing|default","bottom":"var:preset|spacing|3-xl","left":"var:preset|spacing|default"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--3-xl);padding-right:var(--wp--preset--spacing--default);padding-bottom:var(--wp--preset--spacing--3-xl);padding-left:var(--wp--preset--spacing--default)"><!-- wp:heading {"textAlign":"center","fontSize":"3-xl"} -->
-	<h2 class="wp-block-heading has-text-align-center has-3-xl-font-size"><?php esc_html_e( 'You can touch the next generation of WordPress.', 'cormorant' ); ?></h2>
-	<!-- /wp:heading -->
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() . '/assets/images/cormorant-sample-06.jpg' ); ?>","id":3195,"dimRatio":50,"minHeightUnit":"vh","align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|2-xl","bottom":"0","left":"0","right":"0"}}}} -->
+<div class="wp-block-cover alignfull" style="padding-top:var(--wp--preset--spacing--2-xl);padding-right:0;padding-bottom:0;padding-left:0"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-3195" alt="WordPress 100%GPL Block Theme Cormorant Sample Image 06" src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/cormorant-sample-06.jpg' ); ?>" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"className":"is-style-cormorant-dotted-background-group","layout":{"type":"default"}} -->
+		<div class="wp-block-group alignfull is-style-cormorant-dotted-background-group" style="padding-top:0;padding-bottom:0"><!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|s"}}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--s)"><!-- wp:heading {"textAlign":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|xs","bottom":"var:preset|spacing|xs","left":"var:preset|spacing|xs","right":"var:preset|spacing|xs"}}},"fontSize":"3-xl"} -->
+				<h2 class="wp-block-heading has-text-align-center has-3-xl-font-size" style="padding-top:var(--wp--preset--spacing--xs);padding-right:var(--wp--preset--spacing--xs);padding-bottom:var(--wp--preset--spacing--xs);padding-left:var(--wp--preset--spacing--xs)"><?php esc_html_e( 'You can touch the next generation of WordPress.', 'cormorant' ); ?></h2>
+				<!-- /wp:heading -->
 
-	<!-- wp:paragraph {"align":"center"} -->
-	<p class="has-text-align-center"><?php esc_html_e( 'Cormorant is Simple WordPress Block Theme for building website.', 'cormorant' ); ?></p>
-	<!-- /wp:paragraph -->
+				<!-- wp:paragraph {"align":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|xs","bottom":"var:preset|spacing|xs","left":"var:preset|spacing|xs","right":"var:preset|spacing|xs"}}}} -->
+				<p class="has-text-align-center" style="padding-top:var(--wp--preset--spacing--xs);padding-right:var(--wp--preset--spacing--xs);padding-bottom:var(--wp--preset--spacing--xs);padding-left:var(--wp--preset--spacing--xs)"><?php esc_html_e( 'Cormorant is Simple WordPress Block Theme for building website.', 'cormorant' ); ?></p>
+				<!-- /wp:paragraph -->
 
-	<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-	<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-cormorant-filled-square-button-base"} -->
-		<div class="wp-block-button is-style-cormorant-filled-square-button-base"><a class="wp-block-button__link wp-element-button" href="#"><?php esc_html_e( 'Download on WP.org', 'cormorant' ); ?></a></div>
-		<!-- /wp:button -->
+				<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+				<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-cormorant-outlined-square-button-contrast"} -->
+					<div class="wp-block-button is-style-cormorant-outlined-square-button-contrast"><a class="wp-block-button__link wp-element-button" href="<?php echo esc_url( 'https://wordpress.org/themes/cormorant/' ); ?>"><?php esc_html_e( 'Download on WP.org', 'cormorant' ); ?></a></div>
+					<!-- /wp:button -->
 
-		<!-- wp:button {"className":"is-style-cormorant-outlined-square-button-base"} -->
-		<div class="wp-block-button is-style-cormorant-outlined-square-button-base"><a class="wp-block-button__link wp-element-button" href="#"><?php esc_html_e( 'Learn More', 'cormorant' ); ?></a></div>
-		<!-- /wp:button --></div>
-	<!-- /wp:buttons --></div>
-<!-- /wp:group -->
+					<!-- wp:button {"className":"is-style-cormorant-outlined-square-button-secondary"} -->
+					<div class="wp-block-button is-style-cormorant-outlined-square-button-secondary"><a class="wp-block-button__link wp-element-button" href="<?php echo esc_url( 'https://cormorant.kunokoji.com' ); ?>"><?php esc_html_e( 'Learn More', 'cormorant' ); ?></a></div>
+					<!-- /wp:button --></div>
+				<!-- /wp:buttons --></div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"style":{"color":{"gradient":"linear-gradient(180deg,rgba(237,237,237,0) 0%,rgb(255,255,255) 100%)"}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-background" style="background:linear-gradient(180deg,rgba(237,237,237,0) 0%,rgb(255,255,255) 100%)"></div>
+			<!-- /wp:group --></div>
+		<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
